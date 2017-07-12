aaf51148bd4c917c2cfdfbf318b3d9076f89863c
Marcus Ramberg <marcus.ramberg@gmail.com>
2017-07-12 21:53:13 +0200

    Secrets are not prefixed with pluin_

--- update.sh
+++ update.sh
@@ -4,26 +4,25 @@ if [ -z ${PLUGIN_NAMESPACE} ]; then
   PLUGIN_NAMESPACE="default"
 fi
 
-set
 if [ ! -z ${PLUGIN_KUBERNETES_SERVER} ]; then
   KUBERNETES_SERVER=$PLUGIN_KUBERNETES_SERVER
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_TOKEN} ]; then
-  kubectl config set-credentials default --token=${PLUGIN_KUBERNETES_TOKEN}
+if [ ! -z ${KUBERNETES_TOKEN} ]; then
+  kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_USERNAME} ]; then
-  kubectl config set-credentials default --username=${PLUGIN_KUBERNETES_USERNAME} --password=${PLUGIN_KUBERNETS_PASSWORD}
+if [ ! -z ${KUBERNETES_USERNAME} ]; then
+  kubectl config set-credentials default --username=${KUBERNETES_USERNAME} --password=${KUBERNETS_PASSWORD}
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_CLIENT_CERT} ]; then
-  echo ${PLUGIN_KUBERNETES_CLIENT_CERT} | base64 -d > client.crt
-  echo ${PLUGIN_KUBERNETES_CLIENT_KEY} | base64 -d > client.key
+if [ ! -z ${KUBERNETES_CLIENT_CERT} ]; then
+  echo ${KUBERNETES_CLIENT_CERT} | base64 -d > client.crt
+  echo ${KUBERNETES_CLIENT_KEY} | base64 -d > client.key
   kubectl config set-credentials default --server=${KUBERNETES_SERVER} --client-certificate=client.crt --client-key=client.key
 fi
-if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
-  echo ${PLUGIN_KUBERNETES_CERT} | base64 -d > ca.crt
+if [ ! -z ${KUBERNETES_CERT} ]; then
+  echo ${KUBERNETES_CERT} | base64 -d > ca.crt
   kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
 else
   echo "WARNING: Using insecure connection to cluster"
